ef33ade7472528447e3974ca50a1d7bea2c0e224
Lirrum <1016367415@qq.com>
2026-01-24 12:10:39 +0800

    Delete docs directory

--- docs/javascripts/lirrum.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelectorAll('.lirrum-spoiler').forEach(function(spoiler) {
-        spoiler.addEventListener('click', function(e) {
-            this.classList.toggle('revealed');
-            this.style.transform = 'scale(1.05)';
-            setTimeout(() => { this.style.transform = ''; }, 150);
-        });
-    });
-});
